bee9333d1d727273fba959123528dc883aff978c
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-02 18:13:32 -0300

    chore: add npm run build to deploy my app

---
+++ build/precache-manifest.b22a2196281850b81e5901f229c727fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cd772bd596f9186f7f04b3a6d18d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c03e2879bef4c98d36",
+    "url": "/static/css/main.8caafc5c.chunk.css"
+  },
+  {
+    "revision": "121659274d23df8bc347",
+    "url": "/static/js/2.dbccc8f2.chunk.js"
+  },
+  {
+    "revision": "e1c03e2879bef4c98d36",
+    "url": "/static/js/main.b92711d6.chunk.js"
+  },
+  {
+    "revision": "b0b7652f1ccaa0e5e27d",
+    "url": "/static/js/runtime-main.fc1f3d1c.js"
+  }
+]);
