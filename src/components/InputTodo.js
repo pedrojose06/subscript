4b6c26e0738791766d1621bf4781be30d34a364b
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-02 16:11:44 -0300

    feat: change all components to function components

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,36 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from 'react'
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
-  };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <input type="submit" className="input-submit" value="Submit" />
-      </form>
-    );
+const InputTodo = (props) => {
+  const [state, setState] = useState({
+    title: '',
+  })
+  const onChange = (e) => {
+    setState({
+      [e.target.name]: e.target.value,
+    })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addTodoProps(state.title)
+    setState({
+      title: '',
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={state.title}
+        name="title"
+        onChange={onChange}
+      />
+      <input type="submit" className="input-submit" value="Submit" />
+    </form>
+  )
 }
-export default InputTodo;
+export default InputTodo
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react'
 
-class TodoItem extends React.Component {
-  render() {
-    const completedStyle = {
-      fontStyle: "italic",
-      color: "#d35e0f",
-      opacity: 0.4,
-      textDecoration: "line-through"
-    };
-
-    const { completed, id, title } = this.props.todo;
-
-    return (
-      <li className="todo-item">
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </li>
-    );
+const TodoItem = (props) => {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#d35e0f',
+    opacity: 0.4,
+    textDecoration: 'line-through',
   }
+
+  const { completed, id, title } = props.todo
+
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
+      />
+      <button type="button" onClick={() => props.deleteTodoProps(id)}>
+        Delete
+      </button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+    </li>
+  )
 }
 
-export default TodoItem;
+export default TodoItem
