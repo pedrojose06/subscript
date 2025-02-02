42d3125a9dab5e270fe7a7f9b8abc8096054ed2a
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-02 17:37:49 -0300

    fix: include a validation in each list to verify if is empty

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -4,14 +4,18 @@ import TodoItem from './TodoItem'
 const TodosList = (props) => {
   return (
     <div className="todo-list">
-      {props.todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          handleChangeProps={props.handleChangeProps}
-          deleteTodoProps={props.deleteTodoProps}
-        />
-      ))}
+      {props.todos.length > 0 ? (
+        props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={props.handleChangeProps}
+            deleteTodoProps={props.deleteTodoProps}
+          />
+        ))
+      ) : (
+        <h4>This list is empty</h4>
+      )}
     </div>
   )
 }
